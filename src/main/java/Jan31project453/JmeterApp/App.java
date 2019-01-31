f34eb9c4bb4bea0cb0f0ad9b83377b7b87c1a33e
Training_H2A.03.21 <Training_h2a.03.21@accenture.com>
2019-01-31 16:19:16 +0530

    test

---
+++ src/main/java/Jan31project453/JmeterApp/App.java
@@ -0,0 +1,13 @@
+package Jan31project453.JmeterApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Jan31project453/JmeterApp/AppTest.java
@@ -0,0 +1,20 @@
+package Jan31project453.JmeterApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
